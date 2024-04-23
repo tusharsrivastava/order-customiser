1752d54f04905b336d96d673c95a15f0dc7463ea
Tushar Srivastava <razeildecarte@gmail.com>
2024-04-23 19:14:20 +0530

    Minor UI fixes with 3D viewer

--- src/core/components/GLTFViewer/index.tsx
+++ src/core/components/GLTFViewer/index.tsx
@@ -49,7 +49,7 @@ export const GLTFViewer: FC<PropsWithChildren<GLTFViewerPropsType>> = ({children
                             {children}
                             <Environment preset="city" />
                             <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
-                            <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={false} />
+                            <OrbitControls enableZoom={true} enablePan={false} enableRotate={true} />
                         </Suspense>
                     </Canvas>
                 </Grid>
